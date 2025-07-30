3367bb780b3644d1be7a555b1b728b131d8ff2d0
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-30 14:11:47 +0200

    changes

--- handle_manager.h
+++ handle_manager.h
@@ -8,28 +8,34 @@
 extern "C" {
 #endif
 
-#define DECLARE_HANDLE_MANAGER(HANDLE_NAME, HANDLE_KEY, HANDLE_VALUE, NUM_HANDLES) \
-                                                      \
-    void HANDLE_NAME##_manager_set(HANDLE_KEY key, HANDLE_VALUE value);    \
-                                                      \
+#define DECLARE_HANDLE_MANAGER(HANDLE_NAME,                             \
+                               HANDLE_KEY,                              \
+                               HANDLE_VALUE,                            \
+                               NUM_HANDLES)                             \
+                                                                        \
+    void HANDLE_NAME##_manager_set(HANDLE_KEY key, HANDLE_VALUE value); \
+                                                                        \
     HANDLE_VALUE HANDLE_NAME##_manager_get(HANDLE_KEY key);
 
-#define DEFINE_HANDLE_MANAGER(HANDLE_NAME, HANDLE_KEY, HANDLE_VALUE, NUM_HANDLES)      \
+#define DEFINE_HANDLE_MANAGER(HANDLE_NAME,                                \
+                              HANDLE_KEY,                                 \
+                              HANDLE_VALUE,                               \
+                              NUM_HANDLES)                                \
     static HANDLE_VALUE HANDLE_NAME##_manager_handles[NUM_HANDLES] = {0}; \
-                                                          \
-    void HANDLE_NAME##_manager_set(HANDLE_KEY key, HANDLE_VALUE value)         \
-    {                                                     \
-        assert(key < (NUM_HANDLES));                       \
-        assert(!HANDLE_NAME##_manager_handles[key]);              \
-        assert(value);                                    \
-        HANDLE_NAME##_manager_handles[key] = value;               \
-    }                                                     \
-                                                          \
-    HANDLE_VALUE NAME##_manager_get(HANDLE_KEY key)                     \
-    {                                                     \
-        assert(key < (NUM_HANDLES));                       \
-        assert(HANDLE_NAME##_manager_values[key]);               \
-        return HANDLE_NAME##_manager_values[key];                \
+                                                                          \
+    void HANDLE_NAME##_manager_set(HANDLE_KEY key, HANDLE_VALUE value)    \
+    {                                                                     \
+        assert(key < (NUM_HANDLES));                                      \
+        assert(!HANDLE_NAME##_manager_handles[key]);                      \
+        assert(value);                                                    \
+        HANDLE_NAME##_manager_handles[key] = value;                       \
+    }                                                                     \
+                                                                          \
+    HANDLE_VALUE NAME##_manager_get(HANDLE_KEY key)                       \
+    {                                                                     \
+        assert(key < (NUM_HANDLES));                                      \
+        assert(HANDLE_NAME##_manager_values[key]);                        \
+        return HANDLE_NAME##_manager_values[key];                         \
     }
 
 #ifdef __cplusplus
